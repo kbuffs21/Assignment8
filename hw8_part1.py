ef4ba670636f4d8a5e28e8ab5f1385a392e08d7e
kbuffs21 <kema0420@colorado.edu>
2015-11-13 16:48:25 -0700

    a

--- hw8_part1.py
+++ hw8_part1.py
@@ -3,11 +3,26 @@ import operator
 import re
 import string
 
+def print_dict(dictionary, ident = '', braces=1):
+	
+	for key, hAsh in dictionary.iteritems():
+		if isinstance(hAsh, dict):
+			print '%s%s%s%s' %(ident,braces*'[',key,braces*']') 
+			print_dict(hAsh, ident+'  ', braces+1)
+		else:
+			print ident+'%s = %s' %(key, hAsh)
 
 def main():
 	alpha_count_num = dict.fromkeys(string.ascii_lowercase, float(1.0))
 	alpha_count_denom = dict.fromkeys(string.ascii_lowercase, float(26.0))
 	alpha_count_prob = dict.fromkeys(string.ascii_lowercase, float(0.0))
+	#alpha_transition = dict.fromkeys(string.ascii_lowercase, dict.fromkeys(string.ascii_lowercase, float(1.0)))
+	alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+	alpha_transition2 = {}
+	for let1 in alpha:
+		alpha_transition2[let1] = {}
+		for let2 in alpha:
+			alpha_transition2[let1][let2] = float(1.0)
 	lines = []
 	
 	with open('typos20.data') as f:
@@ -21,13 +36,36 @@ def main():
 				alpha_count_num[lines[i][2]] +=1
 	
 	#calc individual probs
-	for iterate in alpha_count_prob:
-		alpha_count_prob[iterate] = alpha_count_num[iterate] / alpha_count_denom[iterate]
+	for letter in alpha_count_prob:
+		alpha_count_prob[letter] = alpha_count_num[letter] / alpha_count_denom[letter]
+	
+	#calc transitional probs
+	for letter in range(0,len(lines)-1):
+		if re.search('[a-z]',lines[letter][0]) and re.search('[a-z]',lines[letter+1][0]):
+			alpha_transition2[(lines[letter][0])][(lines[letter+1][0])] +=1 
+	#normalize transitional probabilities		
+	for letter in alpha_count_prob:
+		for given in alpha_count_prob:
+			alpha_transition2[letter][given] = alpha_transition2[letter][given] / alpha_count_denom[letter]  
+	
+			
+			
+	print 'Transitional Probabilities'
+	print ''
+	#print_dict(alpha_transition2)
+	for letter in alpha_count_prob:
+		for given in alpha_count_prob:
+			print 'probability next letter is: ' + given + ', given this letter is: ' +letter + ' = ' + str(alpha_transition2[letter][given])
 	
 	
+	print ''
+	print '-----------------------------------'
+	print ''
+	print 'Emission Probabilites'
+	print 'E[t] | X[t]'
+	print ''  		
 	for key, val in alpha_count_prob.iteritems():
-		print key, val
-	
+		print key, val	
 
 if __name__ == "__main__":
     main()
